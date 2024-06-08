320cedd350782369359ca65f2e7b55e33df9846a
ariestategarjr <102168712+ariestategarjr@users.noreply.github.com>
2024-06-08 20:57:18 +0700

    Update product.blade.php

--- search-feature/product.blade.php
+++ search-feature/product.blade.php
@@ -1,1 +1,54 @@
+<script>
+    $(document).ready(function() {
+        var table = $("#user_table").DataTable({
+            "processing": true,
+            "serverSide": true,
+            "order": [],
+            "ajax": {
+                "url": "{{ route('sale.showProductsModalData') }}",
+                "type": "POST",
+                "data": function(d) {
+                    d._token = "{{ csrf_token() }}";
+                    d.search = $('input[type="search"]').val();  // Ambil nilai dari input pencarian bawaan DataTable
+                },
+                "dataSrc": "data"
+            },
+            "columns": [{
+                    "data": "id"
+                }, 
+                {
+                    "data": "barcode"
+                },
+                {
+                    "data": "name"
+                },
+                {
+                    "data": "category_name"
+                }, 
+                {
+                    "data": "stock"
+                },
+                {
+                    "data": "selling_price"
+                },
+                {
+                    "data": null,
+                    "render": function(data, type, full, meta) {
+                        return "<input type='number'>";
+                    }
+                },
+                {
+                    "data": null,
+                    "render": function(data, type, full, meta) {
+                        return "<button class='btn btn-sm btn-primary'>Action</button>";
+                    }
+                }
+            ]
+        });
 
+        // Event listener untuk input pencarian bawaan DataTable
+        $('input[type="search"]').on('keyup change', function() {
+            table.draw();
+        });
+    });
+</script>
