aec2570945037a05caae6b4c49a4230a6c7f0061
ariestategarjr <102168712+ariestategarjr@users.noreply.github.com>
2024-06-08 20:59:38 +0700

    Update SaleController.php

--- search-feature/SaleController.php
+++ search-feature/SaleController.php
@@ -1,1 +1,15 @@
+class SaleController extends Controller
+{
+...
+    public function showProductsModalData()
+    {
+        $products = ProductDatatable::getProducts()
+            ->leftJoin('categories', 'products.category_id', '=', 'categories.id')
+            ->leftJoin('units', 'products.unit_id', '=', 'units.id')
+            ->select('products.*', 'categories.name as category_name', 'units.name as unit_name')
+            ->get();
 
+        return response()->json(['data' => $products]);
+    }
+...
+}
