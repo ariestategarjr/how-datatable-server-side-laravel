a359e2f9ad0c06a7c17ed36cb399d5d4698b7f2e
ariestategarjr <102168712+ariestategarjr@users.noreply.github.com>
2024-06-08 20:36:23 +0700

    Update ProductDataTables.php

--- display-data/ProductDataTables.php
+++ display-data/ProductDataTables.php
@@ -1,1 +1,13 @@
+...
+class ProductDatatable extends Model
+{
+    use HasFactory;
 
+    protected $table = 'products';
+
+    public static function getProducts()
+    {
+        return DB::table('products')->select('*');
+    }
+}
+...
